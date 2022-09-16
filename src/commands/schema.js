e53edf66a715d2d90080d687e01c5a3152e330bc
Jiajun Yan <domo36k@gmail.com>
2022-09-16 11:20:51 +0800

    修复创建数据表时 pointer 失效的问题

--- src/commands/schema.js
+++ src/commands/schema.js
@@ -2,7 +2,7 @@ import path from 'path'
 import fs from 'fs'
 import prettyjson from 'prettyjson'
 
-import {usageError, ensureAuth, validateJSON} from '../utils'
+import { usageError, ensureAuth, validateJSON } from '../utils'
 
 const OPERATION_TYPE = {
   CREATE: 'create'
@@ -16,7 +16,7 @@ const OPERATION_TYPE = {
  * @param {*} fields 索引
  */
 const createIndex = async (engine, schemaId, schemaIndex) => {
-  return await engine.request({
+  return engine.request({
     uri: `/oserve/v2.6/schema/${schemaId}/index/`,
     method: 'POST',
     json: schemaIndex
@@ -29,10 +29,55 @@ const createIndex = async (engine, schemaId, schemaIndex) => {
  * @param {*} schemaId 数据表 id
  */
 const removeSchema = async (engine, schemaId) => {
-  return await engine.request({
+  return engine.request({
     uri: `/oserve/v1.8/table/${schemaId}/`,
     method: 'DELETE'
   })
+}
+
+/**
+ * 获取数据表列表
+ * @param {*} engine
+ */
+const getSchemaList = async engine => {
+  return engine.request({
+    uri: '/oserve/v1.8/table/?limit=1000',
+    method: 'GET'
+  })
+}
+
+/**
+ * 替换 pointer 指向的表名为 id
+ * @param {*} engine
+ * @param {*} schemaConfig
+ */
+const replacePointerSchemaWithId = async (engine, schemaConfig) => {
+  if (!schemaConfig.schema || !schemaConfig.schema.fields) {
+    throw usageError('缺少 schema.fields 字段')
+  }
+
+  const schemaList = await getSchemaList(engine)
+  schemaList.body = JSON.parse(schemaList.body)
+
+  schemaConfig.schema.fields.forEach(item => {
+    if (item.type === 'reference') {
+      const [correspondingSchema] = schemaList.body.objects.filter(schemaItem =>
+        [schemaItem.name, schemaItem.id].includes(item.schema_name)
+      )
+
+      if (!correspondingSchema) {
+        throw usageError(
+          `找不到 ${item.name} 字段指向的 ${item.schema_name} 表 id`
+        )
+      }
+
+      item.schema_id = correspondingSchema.id
+
+      delete item.schema_name
+    }
+  })
+
+  return schemaConfig
 }
 
 export const cli = ensureAuth(
@@ -68,11 +113,13 @@ export const cli = ensureAuth(
     }
 
     const fileContent = fs.readFileSync(targetFile, 'utf8')
-    const schemaConfig = validateJSON(fileContent)
+    let schemaConfig = validateJSON(fileContent)
 
     if (!schemaConfig) {
       throw usageError('数据表 JSON 格式错误')
     }
+
+    schemaConfig = await replacePointerSchemaWithId(engine, schemaConfig)
 
     const response = await engine.request({
       uri: '/oserve/v1.8/table/',
